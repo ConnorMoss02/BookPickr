76ba9403f28b7c2d6f60c7d0165e2f56c23c2b5f
ConnorMoss02 <connor.moss@traildogsoftware.com>
2025-09-07 16:20:32 -0400

    Added updated JSON

--- BookPickr/src/vite-env.d.ts
+++ BookPickr/src/vite-env.d.ts
@@ -1,1 +1,32 @@
 /// <reference types="vite/client" />
+declare module '*.css';
+
+// Let TS accept image imports (Vite will handle bundling them)
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+declare module '*.avif' {
+  const src: string;
+  export default src;
+}
